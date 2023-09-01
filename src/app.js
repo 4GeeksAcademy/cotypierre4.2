ee11d5552a550757cf63a0d9c1419d2e837fa050
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-09-01 11:44:23 +0000

    codigo renderizado

--- src/app.js
+++ src/app.js
@@ -44,20 +44,35 @@ import "./assets/img/4geeks.ico";
 
 //Codigo mejorado
 
-const excuses = [
-  "The dog ate my homework before the class.",
-  "My grandma peed my homework right on time.",
-  "His turtle crushed my homework when I finished.",
-  "My bird broke my homework during my lunch.",
-  "I was praying when the dog ate my homework."
-];
-
-function generateExcuse() {
-  return excuses[Math.floor(Math.random() * excuses.length)];
-}
-
 window.onload = () => {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  const btn = document.createElement("button");
+  btn.textContent = "Generate Excuse";
+  btn.addEventListener("click", () => {
+    const excuse = generateExcuse();
+
+    document.querySelector("#the-excuse").innerHTML = excuse;
   });
+
+  document.body.appendChild(btn);
+  console.log("Hello");
+};
+
+let generateExcuse = () => {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const whatIndex = Math.floor(Math.random() * what.length);
+  const whenIndex = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
 };
